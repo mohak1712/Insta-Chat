17f80653c07d93700900c81ec20c4623b8e6d737
Mohak <mohakpuri1712@gmail.com>
2017-03-06 16:38:11 +0530

    Notification Service

--- app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/NotificationReader.java
+++ app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/NotificationReader.java
@@ -3,6 +3,7 @@ package social.chat.whatsapp.fb.messenger.messaging;
 import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
@@ -29,7 +30,7 @@ public class NotificationReader extends NotificationListenerService {
     /**
      * Store notifications in a list
      */
-    ArrayList<NotificationModel> msgs = new ArrayList<>();
+    ArrayList<NotificationModel> msgs;
 
     /**
      * split content to get username and the text received
@@ -40,110 +41,90 @@ public class NotificationReader extends NotificationListenerService {
     /**
      * clear duplicate 1st element from list
      */
-    private boolean clearDuplicate = false;
+    private boolean clearDuplicate;
+
+
     private HashMap<String, ArrayList<NotificationModel>> dataSet = new HashMap<>();
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-//        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-//        inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-//        DisplayMetrics displaymetrics = new DisplayMetrics();
-//        windowManager.getDefaultDisplay().getMetrics(displaymetrics);
-//        heightOfDev = displaymetrics.heightPixels;
-//        widthOfDev = displaymetrics.widthPixels;
-//
-//        addRemoveView();
-//        addBubbleView();
+        Log.d("Lines", "Created");
+        msgs = new ArrayList<>();
     }
-
-
-//    private void addRemoveView() {
-//
-//        removeView = (LinearLayout) inflater.inflate(R.layout.remove_bubble, null);
-//        removeBubble = (ImageView) removeView.findViewById(R.id.removeImg);
-//        WindowManager.LayoutParams paramRemove = new WindowManager.LayoutParams(
-//                WindowManager.LayoutParams.WRAP_CONTENT,
-//                WindowManager.LayoutParams.WRAP_CONTENT,
-//                WindowManager.LayoutParams.TYPE_PHONE,
-//                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-//                PixelFormat.TRANSLUCENT);
-//        paramRemove.gravity = Gravity.TOP | Gravity.LEFT;
-//        removeView.setVisibility(View.GONE);
-//        windowManager.addView(removeView, paramRemove);
-//    }
-
-
-//    private void addBubbleView() {
-//
-//        bubbleView = (LinearLayout) inflater.inflate(R.layout.bubble_layout, null);
-//        bubble = (ImageView) bubbleView.findViewById(R.id.bubble);
-//        bubble.setImageResource(R.drawable.circle_cross);
-//
-//        WindowManager.LayoutParams paramAdd = new WindowManager.LayoutParams(
-//                WindowManager.LayoutParams.WRAP_CONTENT,
-//                WindowManager.LayoutParams.WRAP_CONTENT,
-//                WindowManager.LayoutParams.TYPE_PHONE,
-//                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-//                PixelFormat.TRANSLUCENT);
-//        paramAdd.gravity = Gravity.TOP | Gravity.LEFT;
-//        paramAdd.x = 10;
-//        paramAdd.y = heightOfDev / 2;
-//        windowManager.addView(bubbleView, paramAdd);
-//    }
-
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
 
+        clearDuplicate = false;
 
+        if (!sbn.getPackageName().equals("com.whatsapp"))
+            return;
+
+        Log.d("Lines", "called ji");
         Bundle extras = sbn.getNotification().extras;
         CharSequence[] lines = extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
 
+
         if (lines != null) {
 
-            if (clearDuplicate) {
+            /* app is already running and new notification pops up */
 
-                msgs.clear();
-                clearDuplicate = false;
+            if (msgs.size() != 0) {
+
+                if (msgs.size() >= 7) {
+
+                    msgBifercator = lines[lines.length - 1].toString().split(":");
+                    NotificationModel model = new NotificationModel();
+                    model.setUserName(msgBifercator[0].trim());
+                    model.setMsg(msgBifercator[1].trim());
+                    model.setTime(sbn.getPostTime());
+                    Log.d("Lines", " 7 " + model.getUserName() + " : " + model.getMsg());
+                    msgs.add(model);
+
+                } else {
+
+                    for (int i = msgs.size(); i < lines.length; i++) {
+
+                        msgBifercator = lines[i].toString().split(":");
+                        NotificationModel model = new NotificationModel();
+                        model.setUserName(msgBifercator[0].trim());
+                        model.setMsg(msgBifercator[1].trim());
+                        model.setTime(sbn.getPostTime());
+                        Log.d("Lines", " 1 " + model.getUserName() + " : " + model.getMsg());
+                        msgs.add(model);
+                    }
+
+                }
+
+            } else {
+
+                /* build notification list from scratch */
+
+                for (CharSequence singleLine : lines) {
+
+                    msgBifercator = singleLine.toString().split(":");
+                    NotificationModel model = new NotificationModel();
+                    model.setUserName(msgBifercator[0].trim());
+                    model.setMsg(msgBifercator[1].trim());
+                    model.setTime(sbn.getPostTime());
+                    Log.d("Lines", " 0 " + model.getUserName() + " : " + model.getMsg());
+                    msgs.add(model);
+                }
 
             }
-
-            for (CharSequence singleLine : lines) {
-
-                msgBifercator = singleLine.toString().split(":");
-                NotificationModel model = new NotificationModel();
-                model.setUserName(msgBifercator[0].trim());
-                model.setMsg(msgBifercator[1].trim());
-                Log.d("Lines", model.getUserName() + " : " + model.getMsg());
-                msgs.add(model);
-            }
-
-        } else {
-
-            /* for only single message notification */
-
-            String title = extras.getString("android.title");
-            String text = (String) extras.getCharSequence("android.text");
-
-            if (text != null && title != null) {
-
-                NotificationModel model = new NotificationModel();
-                model.setUserName(title.trim());
-                model.setMsg(text.trim());
-                msgs.add(model);
-                clearDuplicate = true;
-
-            }
-
 
         }
 
-        Log.d("Lines", "m " + msgs.size());
+//        if (extras.containsKey(Notification.EXTRA_PICTURE)) {
+//            // this nf contain the picture attachment
+//            Bitmap bmp = (Bitmap) extras.get(Notification.EXTRA_PICTURE);
+//            Log.d("Lines", "true");
+//        }
 
+        Log.d("Lines", "m s " + msgs.size());
         Intent intent = new Intent(Constants.action);
-        intent.putParcelableArrayListExtra(Constants.msgs,msgs);
+        intent.putParcelableArrayListExtra(Constants.msgs, msgs);
         sendBroadcast(intent);
 
     }
@@ -152,5 +133,7 @@ public class NotificationReader extends NotificationListenerService {
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
         super.onNotificationRemoved(sbn);
+        msgs.clear();
     }
+
 }
