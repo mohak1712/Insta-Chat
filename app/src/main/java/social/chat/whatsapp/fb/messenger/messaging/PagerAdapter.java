11d6d52bdc1324c7e31d2f1748b67695dea8bc47
Mohak <mohakpuri1712@gmail.com>
2017-03-06 16:39:00 +0530

    ViewPager Adapter

---
+++ app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/PagerAdapter.java
@@ -0,0 +1,42 @@
+package social.chat.whatsapp.fb.messenger.messaging;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mohak on 25/2/17.
+ */
+
+class PagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragmentList = new ArrayList<>();
+    private List<String> mFragmentTitleList = new ArrayList<>();
+
+    public PagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
