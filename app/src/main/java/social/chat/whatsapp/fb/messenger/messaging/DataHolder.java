c10c6616db9ea412c0903fafc5d54a4f52eeaf46
Mohak <mohakpuri1712@gmail.com>
2017-04-01 21:30:24 +0530

    add url click functionality

--- app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/DataHolder.java
+++ app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/DataHolder.java
@@ -1,19 +1,30 @@
 package social.chat.whatsapp.fb.messenger.messaging;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.media.Image;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.util.Patterns;
 import android.view.Gravity;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import com.amulyakhare.textdrawable.TextDrawable;
+
+import org.greenrobot.eventbus.EventBus;
+
 import java.util.ArrayList;
-import java.util.Date;
-
-import de.hdodenhof.circleimageview.CircleImageView;
+import java.util.List;
+import java.util.regex.Matcher;
 
 /**
  * Created by mohak on 7/3/17.
@@ -21,14 +32,14 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class DataHolder extends RecyclerView.ViewHolder {
 
     public TextView message, time;
-    public CircleImageView imageView;
+    public ImageView imageView;
     public LinearLayout dataLinear;
 
     public DataHolder(View view) {
         super(view);
         time = (TextView) view.findViewById(R.id.time);
         message = (TextView) view.findViewById(R.id.message);
-        imageView = (CircleImageView) view.findViewById(R.id.icon);
+        imageView = (ImageView) view.findViewById(R.id.icon);
         dataLinear = (LinearLayout) itemView.findViewById(R.id.linear_data);
     }
 
@@ -39,14 +50,18 @@ public class DataHolder extends RecyclerView.ViewHolder {
         params.setMargins(0, 10, 0, 10);
         dataLinear.setLayoutParams(params);
         imageView.setVisibility(View.VISIBLE);
-        message.setBackgroundColor(Color.parseColor("#065E52"));
+//        message.setBackgroundColor(Color.parseColor("#065E52"));
 
         if (!notificationModel.getGroup().equals("-null_123"))
             message.setText(notificationModel.getUserName() + " : \n" + notificationModel.getMsg());
         else
             message.setText(notificationModel.getMsg());
 
-        imageView.setImageBitmap(notificationModel.getIcon());
+        TextDrawable drawable = TextDrawable.builder()
+                .buildRound("" + notificationModel.getUserName().charAt(0), Color.parseColor("#065E52"));
+
+        imageView.setImageDrawable(drawable);
+        addClickToLinks(message.getText().toString());
 
     }
 
@@ -58,6 +73,44 @@ public class DataHolder extends RecyclerView.ViewHolder {
         dataLinear.setLayoutParams(params);
         imageView.setVisibility(View.GONE);
         message.setText(replymessage.getMessage());
-        message.setBackgroundColor(Color.parseColor("#0DC143"));
+        addClickToLinks(replymessage.getMessage());
+//        message.setBackgroundColor(Color.parseColor("#80065E52"));
     }
+
+
+    /**
+     * extracts link from the message if any and sets click on the link
+     * @param text message text
+     */
+    public void addClickToLinks(String text) {
+
+        SpannableString ss = new SpannableString(text);
+        Matcher m = Patterns.WEB_URL.matcher(text);
+        while (m.find()) {
+            final String url = m.group();
+
+            ClickableSpan spannable = new ClickableSpan() {
+                @Override
+                public void onClick(View view) {
+
+                    EventBus.getDefault().post(new closeBubbleEvent());
+
+                    Uri webpage = Uri.parse(url);
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                    webIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    itemView.getContext().startActivity(webIntent);
+
+                }
+            };
+
+            ss.setSpan(spannable, m.start(), m.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        }
+
+        message.setText(ss);
+        message.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+
 }
