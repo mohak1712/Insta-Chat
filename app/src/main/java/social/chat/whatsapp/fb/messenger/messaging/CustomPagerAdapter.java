378a5f12131dc87edd891034f9390204d3eba9a0
Mohak <mohakpuri1712@gmail.com>
2017-04-01 13:58:12 +0530

    copy paste functionality added

--- app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/CustomPagerAdapter.java
+++ app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/CustomPagerAdapter.java
@@ -1,14 +1,22 @@
 package social.chat.whatsapp.fb.messenger.messaging;
 
+import android.content.ClipboardManager;
 import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.*;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+
+import static android.content.Context.CLIPBOARD_SERVICE;
 
 /**
  * Created by mohak on 25/2/17.
@@ -17,18 +25,62 @@ import java.util.List;
 public class CustomPagerAdapter extends PagerAdapter {
 
     private Context mContext;
+    private LinkedHashMap<String, ArrayList<Object>> data;
+    private ArrayList<String> keys;
+    private ListAdapter adapter;
+    private Clicked clickListner;
 
-    public CustomPagerAdapter(Context context) {
-        mContext = context;
+    public CustomPagerAdapter(Context mContext, LinkedHashMap<String, ArrayList<Object>> data, ArrayList<String> keys) {
+        this.mContext = mContext;
+        this.data = data;
+        this.keys = keys;
     }
 
     @Override
-    public Object instantiateItem(ViewGroup collection, int position) {
-        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+    public Object instantiateItem(ViewGroup collection, final int position) {
+
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        ViewGroup layout = (ViewGroup) inflater.inflate(customPagerEnum.getLayoutResId(), collection, false);
+        View layout = inflater.inflate(R.layout.chat_layout, collection, false);
+
+        RecyclerView recyclerView = (RecyclerView) layout.findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+
+        adapter = new ListAdapter(data.get(keys.get(position)), mContext);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setTag(position);
+
+        final EditText editText = (EditText) layout.findViewById(R.id.text);
+
+        ImageView send = (ImageView) layout.findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (clickListner != null)
+                    clickListner.itemClicked(position, editText.getText().toString());
+            }
+        });
+
+        editText.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                ClipboardManager clipMan = (ClipboardManager) mContext.getSystemService(CLIPBOARD_SERVICE);
+                if (clipMan.getPrimaryClip()!=null)
+                    editText.setText(clipMan.getPrimaryClip().getItemAt(0).getText().toString());
+
+                return true;
+            }
+        });
+
         collection.addView(layout);
+
         return layout;
+    }
+
+    public void setItemClickListner(Clicked clickListner) {
+
+        this.clickListner = clickListner;
     }
 
     @Override
@@ -38,7 +90,8 @@ public class CustomPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return CustomPagerEnum.values().length;
+
+        return data.keySet().size();
     }
 
     @Override
@@ -46,10 +99,31 @@ public class CustomPagerAdapter extends PagerAdapter {
         return view == object;
     }
 
-    @Override
-    public CharSequence getPageTitle(int position) {
-        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
-        return mContext.getString(customPagerEnum.getTitleResId());
-    }
+//    public void updateView(String key, ArrayList<NotificationModel> models) {
+//
+//        keys.add(key);
+//        data.put(key, models);
+//        notifyDataSetChanged();
+//        adapter = new ListAdapter(models,mContext);
+//
+//    }
 
+//    public void updatePager(LinkedHashMap<String, ArrayList<NotificationModel>> newData , ArrayList<String> newkey) {
+//
+//        if (data!=null && keys!=null){
+//
+//            keys.clear();
+//            data.clear();
+//
+//            keys.addAll(newkey);
+//            data.putAll(newData);
+//
+//            notifyDataSetChanged();
+//        }
+//    }
+
+    public interface Clicked {
+
+        void itemClicked(int pos, String message);
+    }
 }
