be2e721d98b62e7b492ce771338617904678e0ce
Mohak <mohakpuri1712@gmail.com>
2017-04-03 14:22:58 +0530

    runtime permissions added

--- app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/MainActivity.java
+++ app/src/main/java/social/chat/whatsapp/fb/messenger/messaging/MainActivity.java
@@ -1,12 +1,19 @@
 package social.chat.whatsapp.fb.messenger.messaging;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
 import android.provider.ContactsContract;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,52 +27,120 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     ArrayList<String> data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         data = new ArrayList<>();
+
+        data = new ArrayList<>();
         data.add("WhatsApp");
         data.add("Hangouts");
         data.add("Skype");
+        data.add("Telegram");
+        data.add("Viber");
 
         recyclerView = (RecyclerView) findViewById(R.id.appList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new AppListAdapter(this,data));
+        recyclerView.setAdapter(new AppListAdapter(this, data));
 
-        draw();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1 ) {
-            startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
-        } else
-            startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this))
+            overlayPermission();
+
+        notificationPermission();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_DENIED)
+            readContactsPermission();
 
     }
 
+    private void readContactsPermission() {
 
+        showAlertBox("Read Contacts Permission Required", "This permission is required to read your contacts.Your contacts are not stored." +
+                "Please grant the permission.")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-    private void draw() {
+                        ActivityCompat.requestPermissions(
+                                MainActivity.this,
+                                new String[]{Manifest.permission.READ_CONTACTS},
+                                101);
 
-        if (android.os.Build.VERSION.SDK_INT >= 23) {
-            if (!Settings.canDrawOverlays(this)) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, 101);
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.dismiss();
             }
+        }).show().setCanceledOnTouchOutside(false);
 
-        }
+
 
     }
 
+    private void notificationPermission() {
 
-    public boolean checkNotificationEnabled() {
-        try {
-            return Settings.Secure.getString(getContentResolver(),
-                    "enabled_notification_listeners").contains(getPackageName());
+        ComponentName cn = new ComponentName(this, NotificationReader.class);
+        String flat = Settings.Secure.getString(getContentResolver(), "enabled_notification_listeners");
+        boolean enabled = flat != null && flat.contains(cn.flattenToString());
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
+        if (enabled)
+            return;
+
+        showAlertBox("Read Notification Permission Required", "Please grant the permission.")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1)
+                            startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
+                        else
+                            startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.dismiss();
+            }
+        }).show().setCanceledOnTouchOutside(false);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void overlayPermission() {
+
+        showAlertBox("Overlay Permission Required", "Overlay permission is required so that you can chat from anywhere. Please grant the permission.")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        if (!Settings.canDrawOverlays(MainActivity.this)) {
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                            startActivityForResult(intent, 101);
+                        }
+
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                dialogInterface.dismiss();
+            }
+        }).show().setCanceledOnTouchOutside(false);
+
+    }
+
+    public AlertDialog.Builder showAlertBox(String title, String message) {
+
+        return new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message);
     }
 
 
